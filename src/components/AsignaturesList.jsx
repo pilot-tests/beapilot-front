a18748a80b730d030693b383b8f04a54199875b2
Daniel Matínez <wakkos@gmail.com>
2023-02-07 17:21:47 +0100

    Get it with Axios

--- src/components/AsignaturesList.jsx
+++ src/components/AsignaturesList.jsx
@@ -1,24 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
 import * as API from "../services/getCategories";
 
 export default function AsignaturesList() {
-	const [categories, setCategories] = useState({ results: [] });
-	useEffect(() => {
-		API.getAllCategories().then(setCategories).catch(console.log);
-	}, [])
+	const [data, setData] = useState({ results: [] });
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
 
+
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const response = await axios.get(
+					`http://www.beapilot.local:82/categories`
+				);
+				setData(response.data);
+				setError(null);
+			} catch (err) {
+				setError(err.message);
+				setData(null);
+			} finally {
+				setLoading(false);
+			}
+		};
+		getData();
+	}, []);
 	return (
 		<React.Fragment>
 			<h2>Página de Asignaturas</h2>
+			<h1>API Posts</h1>
+			{loading && <div>A moment please...</div>}
+			{error && (
+				<div>{`There is a problem fetching the post data - ${error}`}</div>
+			)}
 			<ul>
-				{categories.results.map((result) => (
-					<li key={result.id_category}>
-						<Link to={"/test/" + result.id_category}>
-							{result.name_category}
-						</Link>
-					</li>
-				))}
+				{data &&
+					data.results.map((result) => (
+						<li key={result.id_category}>
+							<h3>{result.name_category}</h3>
+						</li>
+					))}
 			</ul>
 		</React.Fragment>
 	);
