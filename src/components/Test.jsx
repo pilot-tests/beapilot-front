e1cb4a8d2ca9b98a6d3767835f9006fdaa9355aa
Daniel Martínez <wakkos@gmail.com>
2023-05-04 11:22:38 +0200

    Create One Question At the time Quiz.

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -7,8 +7,9 @@ export default function Test(props) {
 	const [data, setData] = useState({ results: [] });
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const { testId } = useParams();
-	console.log("Test ID: " + testId)
+
 	const userID = 5;
 
 	useEffect(() => {
@@ -18,11 +19,10 @@ export default function Test(props) {
 
 				// We make sure the test exists
 				const response = await axios.get(
-					`http://www.beapilot.local:82/questionintests?linkTo=id_test_questionintest&equalTo=${ testId }?select=*`
+					`http://www.beapilot.local:82/?examId=${ testId }`
 				);
 
 				setData(response.data);
-				console.log(response.data)
 				setError(null);
 			} catch (err) {
 				setError(err.message);
@@ -33,6 +33,7 @@ export default function Test(props) {
 		};
 		getData();
 	}, []);
+
 		return (
 			<>
 				<div>Página de Test</div>
@@ -40,7 +41,33 @@ export default function Test(props) {
 				{error && (
 					<div>{`There is a problem fetching the post data - ${error}`}</div>
 				)}
-				{data &&
+
+				{data && data.results && (
+					<div >
+						<h2>{data.results[0].string_question}</h2>
+						<ul>
+							<li>
+								{data.results[0].answer_1_string}
+							</li>
+							<li>
+								{data.results[0].answer_2_string}
+							</li>
+							<li>
+								{data.results[0].answer_3_string}
+							</li>
+							<li>
+								{data.results[0].answer_4_string}
+							</li>
+						</ul>
+					</div>
+				)}
+				{/* {data && data.results && (
+        <pre>
+          <code>{JSON.stringify(data.results, null, 2)}</code>
+        </pre>
+      )} */}
+
+				{/* {data &&
 					data.results.map((result) => (
 						<div key={result.id_question_answer}>
 							id_Questionintest: {result.id_questionintest} <br />
@@ -50,7 +77,7 @@ export default function Test(props) {
 							id_User: {result.id_user_questionintest} <br />
 							<hr />
 						</div>
-					))}
+					))} */}
 			</>
 		);
 }
