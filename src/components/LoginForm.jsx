78fa05dc23b6c4af487045694ae14bd812cc7eeb
Daniel Martínez <wakkos@gmail.com>
2023-07-17 15:50:27 +0200

    Test App Type

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -21,7 +21,6 @@ const LoginForm = () => {
           login: true
         },
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
           Auth: import.meta.env.VITE_AUTH
         }
       });
