10d81c5b4d85b1d0d8c71243a6fcbd71887e3140
Daniel Martínez <wakkos@gmail.com>
2023-08-08 12:40:29 +0200

    Correct Typo

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
-import Loader from "./loader/loader"
+import Loader from "./loader/Loader"
 import { useAuth } from '../contexts/AuthContext'
 
 const LoginForm = () => {
